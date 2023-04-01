f1be9dc0815bfa9e5e9cab09957ecf9fa7b26413
Tyler stambaugh <11870675+tylerstambaugh@users.noreply.github.co>
2023-04-01 07:17:12 -0400

    working with hooks is working

--- StravaSegmentSniper.React/ClientApp/src/components/NavMenu.js
+++ StravaSegmentSniper.React/ClientApp/src/components/NavMenu.js
@@ -40,7 +40,7 @@ export class NavMenu extends Component {
                                 <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
                             </NavItem>
                             <NavItem>
-                                <NavLink tag={Link} className="text-dark" to="./components/segment-sniper/main">Segment Sniper</NavLink>
+                                <NavLink tag={Link} className="text-dark" to="/segment-sniper/main">Segment Sniper</NavLink>
                             </NavItem>
                             <LoginMenu>
                             </LoginMenu>
