79244ae0f7826dc6501819edb2e10c2d849f5c4d
Tyler stambaugh <11870675+tylerstambaugh@users.noreply.github.co>
2023-03-31 20:43:41 -0400

    working on react

--- StravaSegmentSniper.React/ClientApp/src/components/segment-sniper/main.js
+++ StravaSegmentSniper.React/ClientApp/src/components/segment-sniper/main.js
@@ -2,32 +2,83 @@ import React, { Component, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import authService from '../api-authorization/AuthorizeService';
 
-export class SegmentSniper extends Component {
 
+function SegmentSniper() {
+
+    const [userName, setUsername] = useState(null);
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+    useEffect(() =>{
+        const _subscription = authService.subscribe(() => this.populateState());
+        populateState();
+
+        return () => {
+            authService.unsubscribe(_subscription);
+          };
+    });
+
+    async function populateState() {
     const [isAuthenticated, user] = await Promise.all([authService.isAuthenticated(), authService.getUser()])
-    // const [isAuthenticated, user] = await Promise.all([authService.isAuthenticated(), authService.getUser()])
-    //this.setState({
-    //    isAuthenticated,
-    //    userName: user && user.name
-    //    });
+    this.setState({
+      isAuthenticated,
+      userName: user && user.name
+    });
+  }
 
-
-render(){
-
-        (user != null) ?
-            <main>
-                <p>this is the segment sniper homepage: {user} is { isAuthenticated}</p>
-            </main>
-
-            : <main>Loading...</main>
-    }
+  if(isAuthenticated) {
+  return (
+    <div>{ userName} is authenticated</div>
+  );
+  } else {
+    return (
+        <div>loading... </div>
+    );
+  }
 }
 
 export default SegmentSniper;
 
 
-//<main>
-//    <RankingGrid items={items} imgArr={imgArr} drag={drag} allowDrop={allowDrop} drop={drop} />
-//    <ItemCollection items={items} drag={drag} imgArr={imgArr} />
-//    <button onClick={Reload} style={{ "marginTop": "10px" }}>Reset</button>
-//</main>
+
+// function MyComponent() {
+//     const [error, setError] = useState(null);
+//     const [isLoaded, setIsLoaded] = useState(false);
+//     const [items, setItems] = useState([]);
+
+//     // Note: the empty deps array [] means
+//     // this useEffect will run once
+//     // similar to componentDidMount()
+//     useEffect(() => {
+//         fetch("https://api.example.com/items")
+//             .then(res => res.json())
+//             .then(
+//                 (result) => {
+//                     setIsLoaded(true);
+//                     setItems(result);
+//                 },
+//                 // Note: it's important to handle errors here
+//                 // instead of a catch() block so that we don't swallow
+//                 // exceptions from actual bugs in components.
+//                 (error) => {
+//                     setIsLoaded(true);
+//                     setError(error);
+//                 }
+//             )
+//     }, [])
+
+//     if (error) {
+//         return <div>Error: {error.message}</div>;
+//     } else if (!isLoaded) {
+//         return <div>Loading...</div>;
+//     } else {
+//         return (
+//             <ul>
+//                 {items.map(item => (
+//                     <li key={item.id}>
+//                         {item.name} {item.price}
+//                     </li>
+//                 ))}
+//             </ul>
+//         );
+//     }
+// }
