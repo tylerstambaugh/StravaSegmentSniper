f12d36710617a6712e47a920272a46470df17f80
Tyler stambaugh <11870675+tylerstambaugh@users.noreply.github.co>
2023-04-27 21:03:06 -0400

    getting rid of boilerplate menuing

--- StravaSegmentSniper.React/ClientApp/src/AppRoutes.js
+++ StravaSegmentSniper.React/ClientApp/src/AppRoutes.js
@@ -1,7 +1,4 @@
 import ApiAuthorzationRoutes from "./components/api-authorization/ApiAuthorizationRoutes";
-import { Counter } from "./components/Counter";
-import { FetchData } from "./components/FetchData";
-import { Home } from "./components/Home";
 import SegmentSniper from "./components/segment-sniper/main";
 import Activity from "./components/segment-sniper/activity/Activity";
 import Athlete from "./components/segment-sniper/athlete/Athlete";
@@ -9,20 +6,7 @@ import Athlete from "./components/segment-sniper/athlete/Athlete";
 const AppRoutes = [
   {
     index: true,
-    element: <Home />,
-  },
-  {
-    path: "/counter",
-    element: <Counter />,
-  },
-  {
-    path: "/fetch-data",
-    requireAuth: true,
-    element: <FetchData />,
-  },
-  {
-    path: "/segment-sniper/*",
-    requireAuth: true,
+    path: "/segment-sniper",
     element: <SegmentSniper />,
   },
   {
