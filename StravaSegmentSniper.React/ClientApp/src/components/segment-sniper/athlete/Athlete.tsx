c16e63e418cab4ffdf1e72153533c6bdf1c33c34
Tyler stambaugh <11870675+tylerstambaugh@users.noreply.github.co>
2023-04-30 16:25:04 -0400

    updates to db

--- StravaSegmentSniper.React/ClientApp/src/components/segment-sniper/athlete/Athlete.tsx
+++ StravaSegmentSniper.React/ClientApp/src/components/segment-sniper/athlete/Athlete.tsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import useGetAthleteDetails from "../hooks/useGetAthleteDetails";
 
 import authService from "../../api-authorization/AuthorizeService";
 import useApiGet from "../hooks/useApiFunction";
 
 const Athlete = () => {
-  function HandleClick(e: any) {
+  const [userName, setUsername] = useState(null);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const _subscription = authService.subscribe(() => populateState());
+    populateState();
+
+    return () => {
+      authService.unsubscribe(_subscription);
+    };
+  });
+
+  async function populateState() {
+    const [isAuthenticated, user] = await Promise.all([
+      authService.isAuthenticated(),
+      authService.getUser(),
+    ]);
+    setIsAuthenticated(isAuthenticated);
+    setUsername(user && user.name);
+  }
+
+  function HandleClick(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
     let response = useGetAthleteDetails();
   }
 
