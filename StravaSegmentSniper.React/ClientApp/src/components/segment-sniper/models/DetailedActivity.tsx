56449d1653e32839fe8b3243b6e481659b6d361c
Tyler stambaugh <11870675+tylerstambaugh@users.noreply.github.co>
2023-05-09 21:26:10 -0400

    mocking data

--- StravaSegmentSniper.React/ClientApp/src/components/segment-sniper/models/DetailedActivity.tsx
+++ StravaSegmentSniper.React/ClientApp/src/components/segment-sniper/models/DetailedActivity.tsx
@@ -1,19 +1,16 @@
+import { StravaMap } from "./ActivityListItem";
 import { SegmentEffort } from "./SegmentEfforts";
 
 type DetailedActivity = {
   name: string;
   distance: number;
   id: string;
+  type: string;
   startDate: Date;
   achievementCount: number;
   startLatLng: [number, number];
   endLatLng: [number, number];
   maxSpeed: number;
+  stravaMap: StravaMap;
   segmentEfforts: SegmentEffort[];
-};
-
-type StravaMap = {
-  id: string;
-  polyline: string;
-  summaryPolyline: string;
 };
