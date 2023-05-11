f014f37ed71d96dec2055a0ac15c19d7620851d3
Tyler stambaugh <11870675+tylerstambaugh@users.noreply.github.co>
2023-05-11 06:42:56 -0400

    ui work on activitylookup

--- StravaSegmentSniper.React/ClientApp/src/App.js
+++ StravaSegmentSniper.React/ClientApp/src/App.js
@@ -4,32 +4,36 @@ import AppRoutes from "./AppRoutes";
 import AuthorizeRoute from "./components/api-authorization/AuthorizeRoute";
 import { Layout } from "./components/Layout";
 import "./custom.css";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
 export default class App extends Component {
   static displayName = "Strava Segment Sniper";
 
   render() {
     return (
-      <Layout>
-        <Routes>
-          {AppRoutes.map((route, index) => {
-            const { element, requireAuth, ...rest } = route;
-            return (
-              <Route
-                key={index}
-                {...rest}
-                element={
-                  requireAuth ? (
-                    <AuthorizeRoute {...rest} element={element} />
-                  ) : (
-                    element
-                  )
-                }
-              />
-            );
-          })}
-        </Routes>
-      </Layout>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <Layout>
+          <Routes>
+            {AppRoutes.map((route, index) => {
+              const { element, requireAuth, ...rest } = route;
+              return (
+                <Route
+                  key={index}
+                  {...rest}
+                  element={
+                    requireAuth ? (
+                      <AuthorizeRoute {...rest} element={element} />
+                    ) : (
+                      element
+                    )
+                  }
+                />
+              );
+            })}
+          </Routes>
+        </Layout>
+      </LocalizationProvider>
     );
   }
 }
