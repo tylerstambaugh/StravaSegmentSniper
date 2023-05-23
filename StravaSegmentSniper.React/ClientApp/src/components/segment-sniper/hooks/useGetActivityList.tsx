bef95730ce1a69fa2f20800d007a37c40ae2bb96
Tyler stambaugh <11870675+tylerstambaugh@users.noreply.github.co>
2023-05-23 06:55:32 -0400

    getting closer?

--- StravaSegmentSniper.React/ClientApp/src/components/segment-sniper/hooks/useGetActivityList.tsx
+++ StravaSegmentSniper.React/ClientApp/src/components/segment-sniper/hooks/useGetActivityList.tsx
@@ -1,25 +1,19 @@
-import { useState } from "react";
 import { ActivityListItem } from "../models/Activity/ActivityListItem";
-import { UseApiCallResponse, useApiGet } from "./useApiGet";
+import { useAxios } from "./useAxios";
 
 const useGetActivityList = async (
   activityId: number
-): Promise<ActivityListItem[] | string> => {
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState<string>();
+): Promise<[boolean, ActivityListItem[] | undefined, string, () => void]> => {
+  const [loading, data, error, request] = useAxios<ActivityListItem[]>({
+    method: "GET",
+    url: `/activity/${activityId}`,
+  });
 
-  async function fetch(activityId: number) {
-    const response = await useApiGet<ActivityListItem[]>(
-      `/activity/${activityId}`
-    );
-    if (response.error) {
-      console.log("something went wrong in useGetActivityList.tsx");
-      setError(response.error);
-    }
-    const responseData: ActivityListItem[] = response.data!;
-    return responseData;
-  }
-  return { fetch, error };
+  const fetch = () => {
+    request();
+  };
+
+  return [loading, data, error, fetch];
 };
 
 export default useGetActivityList;
--- StravaSegmentSniper.React/ClientApp/src/components/segment-sniper/hooks/useGetActivityListWrapper.ts
+++
@@ -1,6 +0,0 @@
-import { useApiGet } from "./useApiGet";
-
-
-const useGetActivityListWrapper = () => {
-    const apiGet = useApiGet();
-}
