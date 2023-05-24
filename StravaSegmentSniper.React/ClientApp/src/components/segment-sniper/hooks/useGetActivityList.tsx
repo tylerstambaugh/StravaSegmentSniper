fa1756ee9e183e65aa3fd853037f3638a73fb513
Tyler stambaugh <11870675+tylerstambaugh@users.noreply.github.co>
2023-05-23 21:18:37 -0400

    closer still

--- StravaSegmentSniper.React/ClientApp/src/components/segment-sniper/hooks/useGetActivityList.tsx
+++ StravaSegmentSniper.React/ClientApp/src/components/segment-sniper/hooks/useGetActivityList.tsx
@@ -1,16 +1,18 @@
 import { ActivityListItem } from "../models/Activity/ActivityListItem";
 import { useAxios } from "./useAxios";
 
-const useGetActivityList = async (
-  activityId: number
-): Promise<[boolean, ActivityListItem[] | undefined, string, () => void]> => {
-  const [loading, data, error, request] = useAxios<ActivityListItem[]>({
-    method: "GET",
-    url: `/activity/${activityId}`,
-  });
+const useGetActivityList = async (): Promise<
+  [
+    boolean,
+    ActivityListItem[] | undefined,
+    string,
+    (activityId: number) => void
+  ]
+> => {
+  const [loading, data, error, request] = useAxios<ActivityListItem[]>();
 
-  const fetch = () => {
-    request();
+  const fetch = (activityId: number) => {
+    request(`/activity/${activityId}`, "GET");
   };
 
   return [loading, data, error, fetch];
