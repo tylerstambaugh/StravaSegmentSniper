6b044a8757cb2ced2b38bcdfccd7470934609233
Tyler Stambaugh <tyler.stambaugh@sevencorners.com>
2023-07-23 09:16:32 -0400

    pt. 2

--- StravaSegmentSniper.React/ActionHandlers/Segment/StarSegmentActionHandler.cs
+++ StravaSegmentSniper.React/ActionHandlers/Segment/StarSegmentActionHandler.cs
@@ -1,4 +1,6 @@
-﻿using StravaSegmentSniper.Services.Internal.Services;
+﻿using StravaSegmentSniper.Services.Internal.Models.Segment;
+using StravaSegmentSniper.Services.Internal.Services;
+using StravaSegmentSniper.Services.StravaAPI.Segment;
 using System.Security.Claims;
 
 namespace StravaSegmentSniper.React.ActionHandlers.Segment
@@ -7,17 +9,23 @@ namespace StravaSegmentSniper.React.ActionHandlers.Segment
     {
         private readonly IWebAppUserService _webAppUserService;
         private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IStravaApiSegment _stravaApiSegment;
 
-        public StarSegmentActionHandler(IWebAppUserService webAppUserService, IHttpContextAccessor httpContextAccessor)
+        public StarSegmentActionHandler(IWebAppUserService webAppUserService, IHttpContextAccessor httpContextAccessor, IStravaApiSegment stravaApiSegment)
         {
             _webAppUserService = webAppUserService;
             _httpContextAccessor = httpContextAccessor;
+            _stravaApiSegment = stravaApiSegment;
         }
 
         public async Task HandleStarSegment(StarSegmentActionHandlerContract contract)
         {
             var user = _webAppUserService.GetLoggedInUserById(_httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier).ToString());
             var userId = user.Id;
+
+            var starSegmentModel = new StarSegmentModel(contract.SegmentId, )
+
+            var result = _stravaApiSegment.StarSegment(userId, )
         }
     }
 }
